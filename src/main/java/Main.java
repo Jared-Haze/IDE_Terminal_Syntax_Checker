ca4a4e35f4b19689e7282cf7dd769c95605380cb
Jared Osei <jaredthomasmitchealoseirosa@gmail.com>
2026-06-11 13:16:07 -0400

    made quick test scanner for user input + slight changes.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,34 @@
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.SequencedMap;
 
 public class Main {
     public static void main(String[] args){
         
+        Scanner scanner = new Scanner(System.in);
+
+        SequencedMap<String, String> testDeck = new LinkedHashMap<>();
+        testDeck.put(Prompts.q1, Prompts.a1);
+        testDeck.put(Prompts.q2, Prompts.a2);
+        testDeck.put(Prompts.q3, Prompts.a3);
+
+        String deckKey1 = testDeck.sequencedKeySet().getFirst();
+        String deckValue1 = testDeck.sequencedValues().getFirst();
+
+        System.out.println(deckKey1);
+        String input = scanner.nextLine();
+
+        if (input.equals(deckValue1)) {
+            System.out.println("correct!");
+        } else {
+            System.out.println("wrong");
+        }
+
+        System.out.println("end of test.");
+    }
+
+    public static void testDeckFirst() {
         SequencedMap<String, String> deck1 = new LinkedHashMap<>();
 
         deck1.put(Prompts.q1, Prompts.a1);
--- src/main/java/Prompts.java
+++ src/main/java/Prompts.java
@@ -1,8 +1,8 @@
 public class Prompts {
     //prompts/questions
-    static String q1 = "type the CLI syntax for staging all files for a Git commit:";
-    static String q2 = "What language is this program written in?";
-    static String q3 = "What build tool is this program using?";
+    static String q1 = "type the CLI syntax for staging all files for a Git commit: ";
+    static String q2 = "What language is this program written in?: ";
+    static String q3 = "What build tool is this program using?: ";
 
     //answers 
     static String a1 = "git add .";
