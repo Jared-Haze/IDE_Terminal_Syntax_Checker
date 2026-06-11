2a2c3f97103eaf24c63ecfdab3df7971c6e7ee1f
Jared Osei <jaredthomasmitchealoseirosa@gmail.com>
2026-06-11 11:58:39 -0400

    Determined data structure for prompts & tested question cycle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.SequencedMap;
+
 public class Main {
     public static void main(String[] args){
         
+        SequencedMap<String, String> deck1 = new LinkedHashMap<>();
+
+        deck1.put(Prompts.q1, Prompts.a1);
+        deck1.put(Prompts.q2, Prompts.a2);
+        deck1.put(Prompts.q3, Prompts.a3);
+
+        System.out.println(deck1.entrySet());
+
+        Map.Entry<String, String> card = deck1.pollFirstEntry();
+        System.out.println(deck1.entrySet());
+
+        deck1.putLast(card.getKey(), card.getValue());
+        System.out.println(deck1.entrySet());
     }
 }
---
+++ src/main/java/Prompts.java
@@ -0,0 +1,11 @@
+public class Prompts {
+    //prompts/questions
+    static String q1 = "type the CLI syntax for staging all files for a Git commit:";
+    static String q2 = "What language is this program written in?";
+    static String q3 = "What build tool is this program using?";
+
+    //answers 
+    static String a1 = "git add .";
+    static String a2 = "Java";
+    static String a3 = "Gradle";
+}
